a2f9a4aefa4d57363111cbb9a9a96c8039c83233
VitalyaNB <vitalya.khomich15@gmail.com>
2026-03-11 19:52:39 +0300

    50x50 tests

---
+++ database/factories/ChatFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ChatFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'last_message_at' => null,
+        ];
+    }
+}
